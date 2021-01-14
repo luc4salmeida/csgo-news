e736ae8e8af03c5610062cfabff0d49c84dfcfa6
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-14 20:26:16 -0300

    Created usecase getLastNews()

---
+++ lib/features/news/domain/usecases/get_last_news.dart
@@ -0,0 +1,17 @@
+import 'package:csgo_flutter/core/error/failure.dart';
+import 'package:csgo_flutter/core/usecases/usecase.dart';
+import 'package:csgo_flutter/features/news/domain/entities/news.dart';
+import 'package:csgo_flutter/features/news/domain/repositories/news_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetLastNews implements UseCase<News, NoParams>
+{
+  final NewsRepository repository;
+
+  GetLastNews(this.repository);
+
+  @override
+  Future<Either<Failure, News>> call(NoParams params) async {
+    return await repository.getLastNews();
+  }
+}
