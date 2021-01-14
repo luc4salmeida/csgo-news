0d7f24e828148de0562dff905cf1eee4d7a2e82e
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-14 20:05:27 -0300

    Implemented specific params to GetMatchByID

--- lib/features/games/domain/usecases/get_game_by_match_id.dart
+++ lib/features/games/domain/usecases/get_game_by_match_id.dart
@@ -1,17 +1,29 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 import '../../../../core/error/failure.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../entities/game.dart';
 import '../repositories/game_repository.dart';
 
-class GetGameByMatchId implements UseCase<Game, String>
+class GetGameByMatchId implements UseCase<Game, Params>
 {
   final GameRepository repository;
   GetGameByMatchId(this.repository);
 
   @override
-  Future<Either<Failure, Game>> call({String params}) {
-    return repository.getGameByMatchId(params);
+  Future<Either<Failure, Game>> call({Params params}) {
+    return repository.getGameByMatchId(params.matchUrl);
   }
+}
+
+class Params extends Equatable
+{
+  final String matchUrl;
+
+  Params({@required this.matchUrl});
+
+  @override
+  List<Object> get props => [matchUrl];
 }
