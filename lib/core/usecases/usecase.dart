5aea2f44488b9ecb2182ba4547ef20251b0b946e
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-14 20:15:52 -0300

    Removed optional params and setup a NoParams class to useCases that don't have params.

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -3,7 +3,7 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> call({Params params});
+  Future<Either<Failure, Type>> call(Params params);
 }
 
 class NoParams extends Equatable {
