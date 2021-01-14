e78408f5965b001d4ebb937b02a3d2c61a02f993
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-14 20:01:01 -0300

    Usecases now uses the abstract UseCase class

--- lib/features/games/domain/usecases/get_game_by_match_id.dart
+++ lib/features/games/domain/usecases/get_game_by_match_id.dart
@@ -1,15 +1,17 @@
 import 'package:dartz/dartz.dart';
-import 'package:meta/meta.dart';
 
 import '../../../../core/error/failure.dart';
+import '../../../../core/usecases/usecase.dart';
 import '../entities/game.dart';
 import '../repositories/game_repository.dart';
 
-class GetGameByMatchId
+class GetGameByMatchId implements UseCase<Game, String>
 {
   final GameRepository repository;
   GetGameByMatchId(this.repository);
 
-  Future<Either<Failure, Game>> execute({@required String url}) async =>
-    await repository.getGameByMatchId(url);
+  @override
+  Future<Either<Failure, Game>> call({String params}) {
+    return repository.getGameByMatchId(params);
+  }
 }
--- lib/features/games/domain/usecases/get_last_games.dart
+++ lib/features/games/domain/usecases/get_last_games.dart
@@ -1,14 +1,17 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/failure.dart';
+import '../../../../core/usecases/usecase.dart';
 import '../entities/game.dart';
 import '../repositories/game_repository.dart';
 
-class GetLastGames
+class GetLastGames implements UseCase<List<Game>, NoParams>
 {
   final GameRepository repository;
   GetLastGames(this.repository);
 
-  Future<Either<Failure, List<Game>>> execute() async =>
-    await repository.getLastGames();
+  @override
+  Future<Either<Failure, List<Game>>> call({NoParams params}) {
+    return repository.getLastGames();
+  }
 }
