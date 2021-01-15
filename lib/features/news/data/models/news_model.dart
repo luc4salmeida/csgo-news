285a54133a26bb405f3584f73c55330218ebf494
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-14 21:53:16 -0300

    Created news model and his test

---
+++ lib/features/news/data/models/news_model.dart
@@ -0,0 +1,29 @@
+import 'package:csgo_flutter/features/news/domain/entities/news.dart';
+
+import 'package:meta/meta.dart';
+
+class NewsModel extends News
+{
+  NewsModel(
+  {
+    @required title,
+    @required description,
+    @required link,
+    @required date
+  }
+  ) : super(
+    title: title,
+    description: description,
+    link: link,
+    date: date
+  );
+
+  factory NewsModel.fromJson(Map<String, dynamic> map) {
+    return NewsModel(
+      date: map['date'],
+      description: map['description'],
+      link: map['link'],
+      title: map['title']
+    );
+  }
+}
---
+++ test/features/news/data/models/news_model_test.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:csgo_flutter/features/news/data/models/news_model.dart';
+import 'package:csgo_flutter/features/news/domain/entities/news.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  final tNewsModel = NewsModel(
+    title: "FURIA add honda as sixth player",
+    description: "The 20-year-old is in for a baptism of fire as he will make his first appearance with the team in the BLAST Premier Global Final.",
+    link: "https://www.hltv.org/news/30981/furia-add-honda-as-sixth-player",
+    date: "Thu, 14 Jan 2021 20:35:00 GMT"
+  );
+
+  test(
+    'should be a subclass of Game entitie', 
+    () async {
+      //assert
+      expect(tNewsModel, isA<News>());
+    }
+  );
+
+  group('fromJson', () {
+    test('should return a valid model when JSON is provided', () async {
+      //arrange
+      final Map<String, dynamic> map = json.decode(fixture('new.json'));
+
+      //act
+      final result = NewsModel.fromJson(map);
+
+      //assert
+      expect(result, tNewsModel);
+    });
+  });
+}
