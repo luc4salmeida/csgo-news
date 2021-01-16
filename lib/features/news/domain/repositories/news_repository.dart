b309bef5198003c5e9d1fc72272c657177b5815a
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-15 22:45:35 -0300

    Changed Data to List<News>

--- lib/features/news/domain/repositories/news_repository.dart
+++ lib/features/news/domain/repositories/news_repository.dart
@@ -3,5 +3,5 @@ import 'package:csgo_flutter/features/news/domain/entities/news.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class NewsRepository {
-  Future<Either<Failure, News>> getLastNews();
+  Future<Either<Failure, List<News>>> getLastNews();
 }
--- lib/features/news/domain/usecases/get_last_news.dart
+++ lib/features/news/domain/usecases/get_last_news.dart
@@ -4,14 +4,14 @@ import 'package:csgo_flutter/features/news/domain/entities/news.dart';
 import 'package:csgo_flutter/features/news/domain/repositories/news_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class GetLastNews implements UseCase<News, NoParams>
+class GetLastNews implements UseCase<List<News>, NoParams>
 {
   final NewsRepository repository;
 
   GetLastNews(this.repository);
 
   @override
-  Future<Either<Failure, News>> call(NoParams params) async {
+  Future<Either<Failure, List<News>>> call(NoParams params) async {
     return await repository.getLastNews();
   }
 }
