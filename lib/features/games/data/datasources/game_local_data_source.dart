62cd0c6caebbe7ec094fdeb4a95780f424cd8704
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-16 20:44:45 -0300

    Implementation of GameLocalDataSource and GameRemoteDataSource

--- lib/features/games/data/datasources/game_local_data_source.dart
+++ lib/features/games/data/datasources/game_local_data_source.dart
@@ -1,4 +1,10 @@
-import 'package:csgo_flutter/features/games/data/models/game_model.dart';
+import 'dart:convert';
+
+import 'package:csgo_flutter/core/error/exceptions.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/game_model.dart';
 
 abstract class GameLocalDataSource
 { /// Gets the cached [List<GameModel>] which was gotten the last time
@@ -15,4 +21,59 @@ abstract class GameLocalDataSource
 
   Future<void> cacheGame(GameModel gameCache);
   Future<void> cacheLastGames(List<GameModel> lastGamesCache);
+}
+
+const LAST_GAMES_CACHED = "LAST_GAMES_CACHED";
+const LAST_GAME_CACHED = "LAST_GAME_CACHED";
+
+class GameLocalDataSourceImpl implements GameLocalDataSource
+{ 
+  final SharedPreferences sharedPreferences;
+
+  GameLocalDataSourceImpl({@required this.sharedPreferences});//this.repository);
+
+  @override
+  Future<List<GameModel>> getLastGames() {
+
+    final cachedString = sharedPreferences.getString(LAST_GAMES_CACHED);
+
+    if(cachedString == null) {
+      throw CacheException();
+    }
+
+    final cachedData = json.decode(cachedString) as List; 
+    final List<GameModel> cachedGames = cachedData.map(
+      (e) => GameModel.fromJson(e)
+    ).toList();
+
+    return Future.value(cachedGames);
+  }
+
+  @override
+  Future<GameModel> getGameByMatchId(String id) {
+    final cachedString = sharedPreferences.getString(LAST_GAME_CACHED);
+
+    if(cachedString == null) {
+      throw CacheException();
+    }
+    final cachedData = json.decode(cachedString);
+    return Future.value(GameModel.fromJson(cachedData));
+  }
+
+  @override
+  Future<void> cacheGame(GameModel gameCache) {
+    return sharedPreferences.setString(
+      LAST_GAME_CACHED, 
+      json.encode(gameCache.toJson())
+    );
+  }
+
+  @override
+  Future<void> cacheLastGames(List<GameModel> lastGamesCache) {
+   return sharedPreferences.setString(
+     LAST_GAMES_CACHED, 
+     json.encode(lastGamesCache.fold<String>("", (previousValue, element) => previousValue + json.encode(element.toJson())))
+    );
+  }
+  
 }
--- lib/features/games/data/datasources/game_remote_data_source.dart
+++ lib/features/games/data/datasources/game_remote_data_source.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
+import 'package:csgo_flutter/core/error/exceptions.dart';
+import 'package:csgo_flutter/features/games/data/models/game_model.dart';
+
 import '../../domain/entities/game.dart';
+import 'package:meta/meta.dart';
+
+import 'package:http/http.dart' as http;
 
 abstract class GameRemoteDataSource
 {
@@ -11,4 +19,42 @@ abstract class GameRemoteDataSource
   /// 
   /// Throws a [ServerException] for all error codes
   Future<List<Game>> getLastGames();
+}
+
+const BASE_API = "https://hltv-api.vercel.app/api/";
+
+class GameRemoteDataSourceImpl implements GameRemoteDataSource
+{
+  final http.Client httpClient;
+
+  GameRemoteDataSourceImpl({@required this.httpClient});
+
+  @override
+  Future<Game> getGameByMatchId(String id) async {
+    final response = await httpClient.get(BASE_API + id);
+
+    if(response.statusCode != 200) {
+      throw ServerException();
+    }
+
+    return GameModel.fromJson(json.decode(response.body));
+  }
+
+  @override
+  Future<List<Game>> getLastGames() async {
+
+    final response = await httpClient.get(BASE_API + 'matches');
+
+    if(response.statusCode != 200) {
+      throw ServerException();
+    }
+
+    final remoteData = json.decode(response.body) as List; 
+    final List<GameModel> remoteGames = remoteData.map(
+      (e) => GameModel.fromJson(e)
+    ).toList();
+
+    return remoteGames;
+  }
+
 }
