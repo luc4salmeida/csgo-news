bca634da38027c95ff7b3a0fb0d08ec51d0f01dd
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-16 21:23:26 -0300

    Implemented NewsLocalDataSource and RemoteLocalDataSource

--- lib/features/news/data/datasources/news_local_data_source.dart
+++ lib/features/news/data/datasources/news_local_data_source.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+
+import 'package:csgo_flutter/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
+
 import '../models/news_model.dart';
 
 abstract class NewsLocalDataSource
@@ -7,6 +14,34 @@ abstract class NewsLocalDataSource
   /// Throws a [CacheException] for all error codes
   Future<List<NewsModel>> getLastNews();
 
-
   Future<void> cacheLastNews(List<NewsModel> lastGamesCache);
+}
+
+const LAST_NEWS_CACHED = "LAST_NEWS_CACHED";
+
+class NewsLocalDataSourceImpl implements NewsLocalDataSource
+{
+  final SharedPreferences sharedPreferences;
+
+  NewsLocalDataSourceImpl({@required this.sharedPreferences});
+
+  @override
+  Future<List<NewsModel>> getLastNews() async {
+    final cachedString = sharedPreferences.getString(LAST_NEWS_CACHED);
+
+    if(cachedString == null) {
+      throw CacheException();
+    }
+
+    final cachedNews = json.decode(cachedString) as List;
+    return cachedNews.map<NewsModel>((e) => NewsModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<void> cacheLastNews(List<NewsModel> lastGamesCache) {
+    return sharedPreferences.setString(
+      LAST_NEWS_CACHED, 
+      json.encode(lastGamesCache.fold("", (previousValue, element) => json.encode(element.toJson())))
+    );
+  }
 }
--- lib/features/news/data/datasources/news_remote_data_source.dart
+++ lib/features/news/data/datasources/news_remote_data_source.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
+import 'package:csgo_flutter/core/consts/api.dart';
+import 'package:csgo_flutter/core/error/exceptions.dart';
+
 import '../models/news_model.dart';
+import 'package:meta/meta.dart';
+
+import 'package:http/http.dart' as http;
 
 abstract class NewsRemoteDataSource
 {
@@ -6,4 +14,28 @@ abstract class NewsRemoteDataSource
   /// 
   /// Throws a [ServerException] for all error codes
   Future<List<NewsModel>> getLastNews();
+}
+
+class NewsRemoteDataSourceImpl implements NewsRemoteDataSource
+{
+
+  final http.Client httpClient;
+
+  NewsRemoteDataSourceImpl({@required this.httpClient});
+
+  @override
+  Future<List<NewsModel>> getLastNews() async {
+
+    final response = await httpClient.get(BASE_API + "news");
+
+    if(response.statusCode == 404) {
+      throw ServerException();
+    }
+
+    final remoteData = json.decode(response.body) as List;
+    
+    return remoteData.map<NewsModel>(
+      (e) => NewsModel.fromJson(e)
+    ).toList();
+  }
 }
