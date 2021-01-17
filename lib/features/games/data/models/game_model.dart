bbe252a0034906fb6f1e243afefc82a0e72cdd63
Luc4sAlmeida <lucasalmeida3_@hotmail.com>
2021-01-17 14:36:20 -0300

    Game model changes (api issues)

--- lib/features/games/data/models/game_model.dart
+++ lib/features/games/data/models/game_model.dart
@@ -6,37 +6,58 @@ import 'team_model.dart';
 class GameModel extends Game
 {
   GameModel({
+    @required int id,
+    @required String link,
+
     @required String event,
-    @required String maps,
+    @required String eventCrest,
+    @required String time, 
+    @required int stars,
+
+    @required String map,
     @required TeamModel team1,
     @required TeamModel team2,
-    @required String matchId
-
   }) : super(
+    id: id,
+    link: link,
+
     event: event,
-    maps: maps,
+    eventCrest: eventCrest,
+    stars: stars,
+    time: time,
+
+    map: map,
     team1: team1,
-    team2: team2,
-    matchId: matchId
+    team2: team2
   );
 
   factory GameModel.fromJson(Map<String, dynamic> map) {
     return GameModel(
-      event: map['event'] as String,
-      maps: map['maps'] as String,
-      matchId: map['matchId'] as String,
-      team1: TeamModel.fromJson(map['team1']),
-      team2: TeamModel.fromJson(map['team2'])
+      id: map['id'],
+      link: map['link'],
+      event: map['event']['name'],
+      eventCrest: map['event']['crest'],
+      time: map['time'],
+      map: map['map'],
+      stars: map['stars'],
+      team1: TeamModel.fromJson(map['teams'][0]),
+      team2: TeamModel.fromJson(map['teams'][1])
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      "id": id,
+      "link": link,
       "event": event,
-      "maps": maps,
-      "team1": (team1 as TeamModel).toJson(),
-      "team2": (team2 as TeamModel).toJson(),
-      "matchId": matchId
+      "eventCrest": eventCrest,
+      "map": map,
+      "time": time,
+      "stars": stars,
+      "teams": [
+        (team1 as TeamModel).toJson(),
+        (team2 as TeamModel).toJson(),
+      ]
     };
   }
 }
